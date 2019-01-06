4e75973e76ba11ab39e6054aad97cecc891474ec
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-01-06 16:06:35 +0800

    feat: 增加新增门店管理模块

---
+++ pages/addStore/addStore.js
@@ -0,0 +1,99 @@
+// pages/addStore/addStore.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: ['餐饮', '美容、美发、养生', '旅游', '酒店', '礼品店', '杂货店'],
+    preview: ''
+  },
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  upload() {
+    wx.chooseImage({
+      success: (res) => {
+        console.log(res);
+        const tempFilePaths = res.tempFilePaths;
+
+        this.setData({
+          preview: tempFilePaths[0]
+        })
+        wx.uploadFile({
+          url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {
+            user: 'test'
+          },
+          success(res) {
+            const data = res.data
+            console.log(res)
+            // do something
+          }
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/login/login.js
+++ pages/login/login.js
@@ -5,21 +5,63 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    phoneno: ''
   },
 
   onSubmit(e) {
-    console.log(e)
-    // wx.switchTab({
-    //   url: '../index/index',
-    // })
+    const phoneno = e.detail.value.phoneno;
+    const password = e.detail.value.password;
+    const source = e.detail.target.dataset.source;
+
+    // 缓存账号密码
+    wx.setStorageSync('phoneno', phoneno);
+    if (source === 'login') {
+      this.login(phoneno, password);
+    } else {
+      this.register(phoneno, password);
+    }
+  },
+
+  login(phoneno, password) {
+    wx.showLoading({
+      title: '正在登录',
+      mask: true
+    });
+
+    let timer = setTimeout(function () {
+      wx.hideLoading();
+      clearTimeout(timer);
+      // 登陆成功跳转首页
+      wx.switchTab({
+        url: '../index/index',
+      })
+    }, 500);
+  },
+
+  register(phoneno, password) {
+    wx.showToast({
+      title: '注册成功',
+    })
+
+    setTimeout(function () {
+      // 登陆成功跳转首页
+      wx.switchTab({
+        url: '../index/index',
+      })
+    }, 500);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const phoneno = wx.getStorageSync('phoneno');
 
+    if (phoneno) {
+      this.setData({
+        phoneno: phoneno
+      })
+    }
   },
 
   /**
